4d03dd4e4206ea76809a707e8d33b981fa4fc066
yuritoma <yuritoma4@gmail.com>
2023-12-06 10:15:36 +0900

    init

---
+++ scrcpy/513. $Spinna Coupe (Youtube).bat
@@ -0,0 +1,1 @@
+scrcpy -S --max-size=1024 --max-fps=15 --video-codec=h264 --rotation=0 --turn-screen-off --stay-awake --window-x=100 --window-y=100 --window-width=300 --window-height=600 --disable-screensaver --power-off-on-close --tcpip --serial 5dc040df
